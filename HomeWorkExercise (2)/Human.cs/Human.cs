f8bd8b1bbef83fb7249ccee885e55c1b6f067a94
BillusionXO <danila.banduristovv@gmail.com>
2024-11-20 19:15:09 +0300

    Add files via upload

---
+++ HomeWorkExercise (2)/Human.cs/Human.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace HomeWorkExercise_2_
+{
+    // Класс Human - описывает человека
+    internal class Human
+    {
+    //1. Внутренние поля класса
+    protected string name; // Имя
+    protected string surname; // Фамилия
+    protected bool sex; // Пол: true - мужской ; false - женский
+
+        // 2. Конструктор с 3 параметрами
+        public Human(string name, string surname, bool sex)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.sex = sex;
+        }
+
+        // 3. Свойства для доступа к внутренним полям класса
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Surname
+        { 
+            get { return surname; }
+            set { surname = value; } 
+        }
+        public bool Sex
+        {
+            get { return sex; }
+            set { sex = value; }
+        }
+
+        // 4. Метод Print() - выводит имена полей класса на экран
+        public void Print()
+        {
+            Console.WriteLine("name:{0}", name);
+            Console.WriteLine("surname:{0}", surname);
+            if (sex)
+                Console.WriteLine("sex: Female");
+            else
+                Console.WriteLine("sex: Male");
+        }
+    }
+}
---
+++ HomeWorkExercise (2)/program.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWorkExercise_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Использование экземпляра класса Human
+            Human hm = new Human("Anton", "Chainikov", false);
+            hm.Print();
+
+            // 2. Использование экземпляра класса Citizen
+            Citizen ct = new Citizen("Nikita", "Frolow", false, "A101ksd1233", "Moscow, Billikova 11");
+            Console.WriteLine("------------------------");
+            ct.Print(); // вывести значение полей класса
+
+            // 3. Использование экземпляра класса ForeignCitizen
+            ForeignCitizen fc = new ForeignCitizen("Alekseev", "Aleksey", false, "0230902ADK", "Omsk, pashakova 3", "023329032JJK",
+            Convert.ToDateTime("02.02.2020"), Convert.ToDateTime("03.03.2020"));
+            Console.WriteLine("------------------------");
+            fc.Print();
+        }
+    }
+}
